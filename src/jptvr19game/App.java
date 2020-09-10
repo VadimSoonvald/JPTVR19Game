108b8d1d0bb5ce82d5f387a109a2e5b23d393eef
pupil <pupil@ivkhk.ee>
2020-09-10 10:18:05 +0300

    Добавлен рандом

--- src/jptvr19game/App.java
+++ src/jptvr19game/App.java
@@ -5,6 +5,7 @@
  */
 package jptvr19game;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -15,7 +16,8 @@ public class App {
     public void run(){
         System.out.println("Привет");
         System.out.println("Программа заудмала число, угадай:");
-        int myNumber = 2;
+        Random random = new Random();
+        int myNumber = random.nextInt(5 - 0 + 1) + 0;
         Scanner scanner = new Scanner(System.in);
         int gamerNumber = scanner.nextInt();
         if(myNumber == gamerNumber){
